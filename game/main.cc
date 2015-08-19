95e7d06aa59d18f32929789b454561a3c52f66e6
mpersano <mpr@fzort.org>
2015-08-19 16:51:49 +0000

    basic scaling

--- game/main.cc
+++ game/main.cc
@@ -24,29 +24,34 @@ class game_app : public ggl::app
 public:
 	game_app();
 
-	void init(int width, int height) override;
+	void init(int viewport_width, int viewport_height) override;
 	void update_and_render(float dt) override;
 
 private:
 	std::unique_ptr<app_state> cur_state_;
 	float update_t_;
-	int width_, height_;
+
+	int viewport_width_, viewport_height_;
+	int scene_width_, scene_height_;
 };
 
 game_app::game_app()
 { }
 
 void
-game_app::init(int width, int height)
+game_app::init(int viewport_width, int viewport_height)
 {
-	width_ = width;
-	height_ = height;
+	viewport_width_ = viewport_width;
+	viewport_height_ = viewport_height;
+
+	scene_width_ = 320;
+	scene_height_ = scene_width_*viewport_height_/viewport_width_;
 
 	ggl::res::load_sprite_sheet("sprites/sprites");
 
 	init_levels();
 
-	cur_state_.reset(new in_game_state { width, height });
+	cur_state_.reset(new in_game_state { scene_width_, scene_height_ });
 
 	update_t_ = 0;
 }
@@ -61,13 +66,13 @@ game_app::update_and_render(float dt)
 		update_t_ -= FRAME_INTERVAL;
 	}
 
-	glViewport(0, 0, width_, height_);
+	glViewport(0, 0, viewport_width_, viewport_height_);
 	glClearColor(0, 0, 0, 0);
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(0, width_, 0, height_, -1, 1);
+	glOrtho(0, scene_width_, 0, scene_height_, -1, 1);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
