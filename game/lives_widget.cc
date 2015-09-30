98f9ca5a2a2fb319a98808239ae33ab53277c06f
mpersano <mpr@fzort.org>
2015-09-30 16:16:10 +0000

    forgot this

---
+++ game/lives_widget.cc
@@ -0,0 +1,68 @@
+#include <ggl/gl.h>
+
+#include "debug_gfx.h"
+#include "game.h"
+#include "lives_widget.h"
+
+lives_widget::lives_widget(game& g)
+: widget { g }
+, show_tics_ { 0 }
+{
+	auto& p = game_.get_player();
+
+	respawn_conn_ =
+		p.get_respawn_event().connect(
+			std::bind(&lives_widget::on_player_respawn, this));
+
+	death_conn_ =
+		p.get_death_event().connect(
+			std::bind(&lives_widget::on_player_death, this));
+}
+
+void
+lives_widget::hide()
+{
+	show_tics_ = 0;
+}
+
+void
+lives_widget::show()
+{
+	show_tics_ = 120;
+}
+
+bool
+lives_widget::update()
+{
+	if (show_tics_ > 0)
+		--show_tics_;
+
+	return true;
+}
+
+void
+lives_widget::draw() const
+{
+	if (show_tics_) {
+		glDisable(GL_TEXTURE_2D);
+		glDisable(GL_BLEND);
+
+		glColor4f(1, 0, 0, 1);
+
+		draw_circle(game_.get_player_screen_position(), 30);
+	}
+}
+
+void
+lives_widget::on_player_respawn()
+{
+	printf("respawned!\n");
+	show();
+}
+
+void
+lives_widget::on_player_death()
+{
+	printf("died!\n");
+	hide();
+}
---
+++ game/lives_widget.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <ggl/event.h>
+
+#include "widget.h"
+
+class lives_widget : public widget
+{
+public:
+	lives_widget(game& g);
+
+	void hide() override;
+	void show() override;
+
+	bool update() override;
+	void draw() const override;
+
+private:
+	void on_player_respawn();
+	void on_player_death();
+
+	int show_tics_;
+	ggl::event_connection_ptr respawn_conn_;
+	ggl::event_connection_ptr death_conn_;
+};
